d70724802bffb9d2d8d12a4e4249e39e43aa354d
Rhys Morris <rhysmorris08@gmail.com>
2020-10-22 17:49:53 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Caeser Cipher
+
+Terminal based Caeser Cipher program written in Ruby. 
+
+Caeser Cipher refers to a plaintext encoding method where letters are moved a fixed number of positions down the alphabet. The cipher is named after Juluis Caeser, who used it to encode and decode his personal correspondence. 
+
+### Use
+
+Run program in terminal. 
+
+Enter plaintext to convert and number of postions to rotate through alphabet when prompted in the terminal.
+
+Created as part of coursework for [**The Odin Project**](https://www.theodinproject.com/courses/ruby-programming/lessons/caesar-cipher)
---
+++ caeser-cipher.rb
@@ -0,0 +1,55 @@
+def caesar_cipher(string, rotate)
+
+    # Populate alphabet array
+    alphabet = []
+    for char in "a".."z" do
+        alphabet.push(char)
+    end
+
+    # Split string into individual chars
+    string_array = string.split('')
+
+    def convert_char(alphabet, char, rotate)
+        # If non-alphabet letter return unchanged
+        if !alphabet.include?(char.downcase)
+            return char
+        end
+        
+        # Check whether alphabet letter is upper or lower case
+        upper_case = false
+        if char.upcase == char
+            upper_case = true
+        end
+        
+        # Convert uppercase chars to downcase
+        char.downcase!
+        
+        # Find index in alphabet
+        index = alphabet.find_index(char)
+        new_index = index + rotate
+        # Wrap alphabet
+        if new_index > 25
+            new_index = new_index % 26
+        end
+
+        # Return char in correct case
+        upper_case ? alphabet[new_index].upcase : alphabet[new_index]
+
+    end
+
+    # Convert each char and build return string
+    converted_string = ""
+    for char in string_array do
+        converted_string += convert_char(alphabet, char, rotate)
+    end
+
+    converted_string
+end
+
+# Get use input for conversion
+puts "Enter plaintext to cipher:"
+original = gets.chomp
+puts "Enter cipher rotation:"
+rotation = gets.chomp.to_i
+
+puts caesar_cipher(original, rotation)
